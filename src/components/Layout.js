5a9086ffdaa46a6d815e8de7725c05fd90c746e8
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2021-06-09 10:09:26 +0200

    fic

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,6 @@ import React, { useState } from "react"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
-
 import "../assets/css/main.css"
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false)
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,42 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-const Contact = () => {
+const contact = () => {
   return (
     <Layout>
-      <h1> Contact page testing</h1>
+     
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>get in touch</h3>
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-control"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="form-control"
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="message"
+                className="form-control"
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              submit here
+            </button>
+          </form>
+        </article>
+      </section>
     </Layout>
   )
 }
 
-export default Contact
+export default contact
