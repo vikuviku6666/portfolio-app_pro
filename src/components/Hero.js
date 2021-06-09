a983e628a30ed5586b4e6b976bdb96ab21a1ec55
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2021-06-09 11:48:15 +0200

    sec

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,43 +1,29 @@
 import React from "react"
-import Image from "gatsby-plugin-image"
 import { Link } from "gatsby"
-import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
+import { StaticImage } from "gatsby-plugin-image"
 
-const query = graphql`
-  {
-    file(relativePath: { eq: "resume.png"}) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
 const Hero = () => {
-  // const {
-  //   file: {
-  //     childImageSharp: { fluid },
-  //   },
-  // } = useStaticQuery(query)
-  const data = useStaticQuery(query)
-  console.log(data)
   return (
     <header className="hero">
       <div className="section-center hero-center">
         <article className="hero-info">
           <div>
             <div className="underline"></div>
-            <h1>Vinay</h1>
-            <h4>FullStack Developer</h4>
+            <h1>Fullstack</h1>
+            <h1>Developer</h1>
             <Link to="/contact" className="btn">
               contact me
             </Link>
-           <SocialLinks/>
+            <SocialLinks />
           </div>
         </article>
-        {/* <Image fluid={fluid} className="hero-img" /> */}
+        <StaticImage
+          src="../assets/images/resume.png"
+          alt="portfolio"
+          className="hero-img"
+          placeholder="blurred"
+        />
       </div>
     </header>
   )
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-import { Link } from "gatsby"
+
 
 const Project = ({ description, title, github, stack, url, image, index }) => {
   return (
