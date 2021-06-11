e2db40a42cc49e3448b04b54b137555e8b0166c5
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2021-06-11 08:50:42 +0200

    elc

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -5,18 +5,18 @@ export default [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "FrontEnd Development",
-    text: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature.`,
+    text: `Worked on React, Redux, Typescript, Graphql, SASS, Styled Component, GatsBy, NextJS, Jest`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
     title: "BackEnd Development",
-    text: `From 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.`,
+    text: `Build API with Node and C# and Worked on PostgreSQL, MSSQL and MONGO db, also Build serverless API with AWS`,
   },
   {
     id: 3,
     icon: <FaAndroid className="service-icon" />,
     title: "Devops",
-    text: `Looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature.`,
+    text: `Worked on Git & Github, Linux, Jenkins, CI & CD Pipeline, Azure devops, Docker, Kubernetes, Terraform, AWS, AZURE `,
   },
 ]
--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,43 +1,31 @@
 import React from "react"
 import {
-  FaFacebookSquare,
   FaLinkedin,
-  FaDribbbleSquare,
-  FaBehanceSquare,
+  FaGithub,
   FaTwitterSquare,
 } from "react-icons/fa"
 
 const data = [
   {
     id: 1,
-    icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
+    url: "https://www.linkedin.com/in/vinay-kumar-6666viku/",
   },
   {
     id: 2,
-    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
+    url: "https://twitter.com/Vinaaayyku",
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 5,
-    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaGithub className="social-icon"></FaGithub>,
+    url: "https://github.com/vikuviku6666",
   },
 ]
 const links = data.map(link => {
   return (
     <li key={link.id}>
-      <a href={link.url} className="social-link">
+      <a href={link.url} target="_blank" className="social-link">
         {link.icon}
       </a>
     </li>
